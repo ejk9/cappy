6a7759690227c07174b9ff14c68a194696d53d93
Utaha-Kasumigod <57812344+Utaha-Kasumigod@users.noreply.github.com>
2021-12-25 07:18:59 -0700

    changed format, users change now be created password and username need a cerntain amount of char dont know how to make it require stronger password without more work
    next thing to work on it a user page and knows user is logged in.

--- userTesting/index.js
+++ userTesting/index.js
@@ -2,25 +2,57 @@
 
 
 var gun = GUN();
-var items = gun.get('items');
+// var items = gun.get('items');
+var user = gun.user();
+
+var name = "";
 
 
 
 
 $('form').on('submit', function(e){
     e.preventDefault();
-    items.set($('#userName').val());
-    $('#userName').val("a");
+    $('#error').text("");
+    // items.set($('#userName').val());
+    // $('#userName').val("a");
+    
+    //console.log($('#userName').val());
+    //console.log($('#userName').val().length);
+    
+    var temp = $('#passWord').val().length;
 
-
-});
-
-items.map().on(function(item, id){
-    var li = $('#' + id).get(0) || $('<li>').attr('id', id).appendTo('ul');
-    if(item){
-        $(li).text(item);
-    } else {
-        $(li).hide();
+    if(temp < 8){
+        $('#error').append("Password Not Long Enough\<br\>");
+        $('#passWord').val("");
     }
+
+    if($('#userName').val().length < 3){
+        $('#error').append("Username Not Long Enough");
+        $('#passWord').val("");
+    }
+
+    user.create($('#userName').val(), $('#passWord').val(), function(ack){
+        //console.log($('#userName').val());
+        name = $('#userName').val();
+        window.location.href = "userPage.html";
+    });
+
+
+
+
 });
+
+// $('#passWord').keydown(function(e){
+//     //e.preventDefault();
+//     if($('#passWord').val().length <= 8){
+//         $('#error').val("Password Not Long Enough");
+//     }
+//     if($('#passWord').val().length > 8){
+//         $('#error').val("Password probably okay.");
+//     }
+
+// })
+
+
+
 
